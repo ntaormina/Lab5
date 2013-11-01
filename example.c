1ea7155995f416c311ca7d1bf1a5a7266c48b919
Todd Branchflower <todd.branchflower@gmail.com>
2013-11-01 14:52:19 -0600

    added skeleton main code in example

---
+++ example.c
@@ -0,0 +1,54 @@
+#include <msp430.h>
+#include "game.h"
+#include "button/buttons.h"
+
+void init_timer();
+void init_buttons();
+
+int main(void)
+{
+	WDTCTL = (WDTPW|WDTHOLD);
+
+	unsigned char player = initPlayer();
+
+	init_timer();
+	init_buttons();
+	__enable_interrupt();
+
+	while(1)
+	{
+		/*
+		 * while (game is on)
+		 * {
+		 * 		check if button is pushed (you don't want to block here, so don't poll!)
+		 * 		if button is pushed:
+		 * 			clear current player marker
+		 * 			update player position based on direction
+		 * 			print new player
+		 * 			clear two second timer
+		 * 			wait for button release (you can poll here)
+		 * }
+		 *
+		 * print game over or you won, depending on game result
+		 *
+		 * wait for button press to begin new game (you can poll here)
+		 * wait for release before starting again
+		 */
+	}
+
+	return 0;
+}
+
+// 
+// YOUR TIMER A ISR GOES HERE
+//
+
+void init_timer()
+{
+	// do timer initialization work
+}
+
+void init_buttons()
+{
+	// do button initialization work
+}
