b832669baaa6f722787dfd383394bccf3e08fa12
Nik Taormina <c15nikolas.taormina@usafa.edu>
2013-11-07 23:09:19 -0700

    initial commit

--- game.c
+++ game.c
@@ -1,6 +1,6 @@
-#include <msp430g2553.h>
+ #include <msp430g2553.h>
 #include "game.h"
-#include "LCD/LCD.h"
+#include "../LCD-1/LCD.h"
 
 unsigned char initPlayer()
 {
@@ -19,12 +19,25 @@ void clearPlayer(unsigned char player)
 	writeDataByte(' ');
 }
 
+#define ROW_MASK 0x40
+
 unsigned char movePlayer(unsigned char player, unsigned char direction)
 {
 	switch (direction) {
-		//
-		// update player position based on direction of movement
-		//
+		case RIGHT:
+			if ((player & 0x0f) < 7)
+				player += 1;
+			break;
+		case LEFT:
+			if ((player & 0x0f) > 0)
+				player -= 1;
+			break;
+		case UP:
+			player &= ~ROW_MASK;
+			break;
+		case DOWN:
+			player |= ROW_MASK;
+			break;
 	}
 
 	return player;
